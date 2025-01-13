bf8014649888f83e906ca8677d43c1e8424e5ae8
OguzhanAndic <oguzhan.andic@hotmail.com>
2025-01-13 11:53:02 +0100

    Update module-info.java

--- Schema/src/main/java/module-info.java
+++ Schema/src/main/java/module-info.java
@@ -2,11 +2,14 @@ module com.example.schema {
     requires javafx.controls;
     requires javafx.fxml;
 
-    // Öppna rätt paket för JavaFX FXML-inläsning
+
     opens com.example.schema to javafx.fxml;
+    exports com.example.schema;
+    exports com.example.inlogg;  // Gör paketet tillgängligt för andra moduler
     opens com.example.inlogg to javafx.fxml;
 
-    // Exports är bra för att göra paketet tillgängligt för andra moduler
-    exports com.example.schema;
-    exports com.example.inlogg;
+    opens com.example.hello to javafx.fxml;
+    exports com.example.hello;
+    opens com.example.Events to javafx.fxml;
+    exports com.example.Events;
 }
